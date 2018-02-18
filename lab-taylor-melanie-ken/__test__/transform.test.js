07ac085845db970a595b3bf57228ead27073ace3
Ken Unterseher <kenunterseher@MacBook-Air-3.local>
2018-02-17 16:55:08 -0800

    all tests to transform test

--- lab-taylor-melanie-ken/__test__/transform.test.js
+++ lab-taylor-melanie-ken/__test__/transform.test.js
@@ -29,50 +29,167 @@ describe('File Reader/Writer Module', function() {
   });
 });
 
-describe('Constructor Module', function() {
-  describe('with an improper file path', function() {
-    it('should return an error', function(done) {
-      // assertions
-      constructor(`${__dirname}/notHere.txt`, function(err) {
-        expect(err).toBeTruthy();
-        expect(typeof err).toBe('object');
-        expect(err.code).toBe('ENOENT');
-      });
-      done();
-    });
-  });
-  describe('with the proper file path', function() {
-    it('should return the content of the file', function(done) {
-      //assertions
-      constructor(`${__dirname}/..lib/bitmap-constructor.js`, function(err, data) {
-        expect(err).toBe(null);
-        expect(typeof data).toBe('data');
-      });
-      done();
-    });
-  });
-});
+// describe('Constructor Module', function() {
+//   describe('with an improper file path', function() {
+//     it('should return an error', function(done) {
+//       // assertions
+//       constructor(`${__dirname}/notHere.txt`, function(err) {
+//         expect(err).toBeTruthy();
+//         expect(typeof err).toBe('object');
+//         expect(err.code).toBe('ENOENT');
+//       });
+//       done();
+//     });
+//   });
+//   describe('with the proper file path', function() {
+//     it('should return the content of the file', function(done) {
+//       //assertions
+//       constructor(`${__dirname}/..lib/bitmap-constructor.js`, function(err, data) {
+//         expect(err).toBe(null);
+//         expect(typeof data).toBe('data');
+//       });
+//       done();
+//     });
+//   });
+// });
 
 describe('Transform Module', function() {
-  describe('with an improper file path', function() {
-    it('should return an error', function(done) {
-      // assertions
-      transform.invert(`/notta.txt`, function(err) {
-        expect(err).toBeTruthy();
-        expect(typeof err).toBe('object');
-        expect(err.code).toBe('ENOENT');
+  describe('#invert', function() {
+    describe('with an improper file path', function() {
+      it('should return an error', function(done) {
+        // assertions
+        transform.invert(`${__dirname}/notta.txt`, function(err) {
+          expect(err).toBeTruthy();
+          expect(typeof err).toBe('object');
+          expect(err.code).toBe('ENOENT');
+        });
+        done();
       });
-      done();
+    });
+    describe('with the correct file path', function() {
+      it('should return the content of the file', function(done) {
+        //assertions
+        transform.invert(`${__dirname}/../data/palette-bitmap.bmp`, function(err, data) {
+          expect(err).toBe(null);
+          expect(typeof data).toBe('data');
+        });
+        done();
+      });
     });
   });
-  describe('with the correct file path', function() {
-    it('should return the content of the file', function(done) {
-      //assertions
-      transform.invert(`/../data/palette-bitmap.bmp`, function(err, data) {
-        expect(err).toBe(null);
-        expect(typeof data).toBe('data');
+  describe('#diagonal', function() {
+    describe('with an improper file path', function() {
+      it('should return an error', function(done) {
+        // assertions
+        transform.diagonal(`${__dirname}/notta.txt`, function(err) {
+          expect(err).toBeTruthy();
+          expect(typeof err).toBe('object');
+          expect(err.code).toBe('ENOENT');
+        });
+        done();
       });
-      done();
+    });
+    describe('with the correct file path', function() {
+      it('should return the content of the file', function(done) {
+        //assertions
+        transform.diagonal(`${__dirname}/../data/palette-bitmap.bmp`, function(err, data) {
+          expect(err).toBe(null);
+          expect(typeof data).toBe('data');
+        });
+        done();
+      });
+    });
+  });
+  describe('#toLateEighties', function() {
+    describe('with an improper file path', function() {
+      it('should return an error', function(done) {
+        // assertions
+        transform.toLateEighties(`${__dirname}/notta.txt`, function(err) {
+          expect(err).toBeTruthy();
+          expect(typeof err).toBe('object');
+          expect(err.code).toBe('ENOENT');
+        });
+        done();
+      });
+    });
+    describe('with the correct file path', function() {
+      it('should return the content of the file', function(done) {
+        //assertions
+        transform.toLateEighties(`${__dirname}/../data/palette-bitmap.bmp`, function(err, data) {
+          expect(err).toBe(null);
+          expect(typeof data).toBe('data');
+        });
+        done();
+      });
+    });
+  });
+  describe('#blkToWhite', function() {
+    describe('with an improper file path', function() {
+      it('should return an error', function(done) {
+        // assertions
+        transform.blkToWhite(`${__dirname}/notta.txt`, function(err) {
+          expect(err).toBeTruthy();
+          expect(typeof err).toBe('object');
+          expect(err.code).toBe('ENOENT');
+        });
+        done();
+      });
+    });
+    describe('with the correct file path', function() {
+      it('should return the content of the file', function(done) {
+        //assertions
+        transform.blkToWhite(`${__dirname}/../data/palette-bitmap.bmp`, function(err, data) {
+          expect(err).toBe(null);
+          expect(typeof data).toBe('data');
+        });
+        done();
+      });
+    });
+  });
+  describe('#greyscale', function() {
+    describe('with an improper file path', function() {
+      it('should return an error', function(done) {
+        // assertions
+        transform.greyscale(`${__dirname}/notta.txt`, function(err) {
+          expect(err).toBeTruthy();
+          expect(typeof err).toBe('object');
+          expect(err.code).toBe('ENOENT');
+        });
+        done();
+      });
+    });
+    describe('with the correct file path', function() {
+      it('should return the content of the file', function(done) {
+        //assertions
+        transform.greyscale(`${__dirname}/../data/palette-bitmap.bmp`, function(err, data) {
+          expect(err).toBe(null);
+          expect(typeof data).toBe('data');
+        });
+        done();
+      });
+    });
+  });
+  describe('#redAndBlack', function() {
+    describe('with an improper file path', function() {
+      it('should return an error', function(done) {
+        // assertions
+        transform.redAndBlack(`${__dirname}/notta.txt`, function(err) {
+          expect(err).toBeTruthy();
+          expect(typeof err).toBe('object');
+          expect(err.code).toBe('ENOENT');
+        });
+        done();
+      });
+    });
+    describe('with the correct file path', function() {
+      it('should return the content of the file', function(done) {
+        //assertions
+        transform.redAndBlack(`${__dirname}/../data/palette-bitmap.bmp`, function(err, data) {
+          expect(err).toBe(null);
+          expect(typeof data).toBe('data');
+        });
+        done();
+      });
     });
   });
 });
